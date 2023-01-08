f6eee1a0e125d76b845651d15898cbd4de9704ef
Allen Joseph <allenjos1996@gmail.com>
2023-01-08 19:43:08 +0100

    Swagger

--- src/main/java/de/conrad/codeworkshop/factory/services/order/Service.java
+++ src/main/java/de/conrad/codeworkshop/factory/services/order/Service.java
@@ -96,6 +96,6 @@ public class Service {
                                                }
                                            }
                                    }
-        ).join();
+        );
     }
 }
